a5af3e90bfa4588f3c3d03d62b2bf2bc25a49e69
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2023-07-22 19:39:25 +0200

    Fixed test

--- nestjs/conf/src/chat/gateway/chat/chat.gateway.spec.ts
+++ nestjs/conf/src/chat/gateway/chat/chat.gateway.spec.ts
@@ -3,6 +3,7 @@ import { ChatGateway } from './chat.gateway';
 import { UserService } from '../../../user/service/user-service/user.service';
 import { FriendshipService } from '../../../chat/service/friendship/friendship.service';
 import { ConnectedUserService } from '../../../chat/service/connected-user/connected-user.service';
+import { ChannelService } from '../../../chat/service/channel/channel.service';
 import { AuthService } from '../../../auth/service/auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { DirectMessageService } from '../../../chat/service/direct-message/direct-message.service';
@@ -22,6 +23,7 @@ describe('ChatGateway', () => {
         AuthService,
         DirectMessageService,
         MessageService,
+        ChannelService,
         {
           provide: JwtService,
           useValue: {},
