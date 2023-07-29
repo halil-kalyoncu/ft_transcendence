5bf1a2250ab03442fc6034974086e617de3670a5
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2023-07-29 20:45:04 +0200

    Added conditional stylings to Scrollviewer

--- vue/conf/src/components/utils/ScrollViewer.vue
+++ vue/conf/src/components/utils/ScrollViewer.vue
@@ -1,13 +1,38 @@
 <template>
-  <div class="scrollview" :style="{ 'max-height': maxHeight, 'padding-right': paddingRight }">
+  <div
+    class="scrollview"
+    :style="{ 'max-height': maxHeight, 'padding-right': scrollbarActive ? paddingRight : '0' }"
+    @scroll="handleScroll"
+  >
     <slot></slot>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted, watchEffect } from 'vue'
+
 const props = defineProps({
   maxHeight: String,
   paddingRight: String
+})
+
+let scrollbarActive = ref(false)
+
+const handleScroll = () => {
+  scrollbarActive.value = true
+}
+
+onMounted(() => {
+  watchEffect(() => {
+    const scrollHeight = document.querySelector('.scrollview').scrollHeight
+    const clientHeight = document.querySelector('.scrollview').clientHeight
+
+    scrollbarActive.value = scrollHeight > clientHeight
+  })
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
 })
 </script>
 
