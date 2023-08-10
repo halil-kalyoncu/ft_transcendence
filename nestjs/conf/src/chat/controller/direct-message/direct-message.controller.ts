ed7dd9314a3898ab24c212687ffa8e5fb6aca4e0
Esra Lenz <elenz@1-D-3.42heilbronn.de>
2023-08-10 15:03:18 +0200

    COMMENTS: Added comments to the API-Functions

--- nestjs/conf/src/chat/controller/direct-message/direct-message.controller.ts
+++ nestjs/conf/src/chat/controller/direct-message/direct-message.controller.ts
@@ -14,6 +14,8 @@ export class DirectMessageController {
         private directMessageService: DirectMessageService,
     ) {}
 
+	//Get all the directMessages between two users with the messages and the participants (receiver and sender) from the message table
+	// Needs the readerUserId and the withUserId as a URL from the frontend, may be changed in the future
     @Get("getDirectMessages")
     async getDirectMessages(
         @Query('readerUserId', ParseIntPipe) readerUserId: number,
@@ -27,6 +29,7 @@ export class DirectMessageController {
     //     return this.directMessageService.getAllUnreadMessages(userId);
     // }
 
+	//Get all the unread messages from the db based on the userId given as a URL from the frontend
     @Get('allUnreadByUserId')
     async getAllUnreadDirectMessages(@Query('userId', ParseIntPipe) userId: number): Promise<UnreadMessagesDto[]> {
         const unreadMessages = await this.directMessageService.getAllUnreadMessages(userId);
--- nestjs/conf/src/chat/controller/friendship/friendship.controller.ts
+++ nestjs/conf/src/chat/controller/friendship/friendship.controller.ts
@@ -11,12 +11,12 @@ export class FriendshipController {
         private friendshipService: FriendshipService,
     ) {}
 
-
+    //GET Friends from the db where the status is accepted based on the userId given as a URL from the frontend
     @Get('get-accepted-friends')
     async getAcceptedFriends(@Query('userId', ParseIntPipe) userId: number): Promise<FriendshipDto[]> {
         return this.friendshipService.getFriends(userId);
     }
-
+    //GET friends from the db where the status is pending based o the userId given as a URL from the frontend
     @Get('get-friend-requests')
     async getFriendRequests(@Query('userId', ParseIntPipe) userId: number): Promise<FriendshipDto[]> {
         return this.friendshipService.getFriendRequests(userId);
