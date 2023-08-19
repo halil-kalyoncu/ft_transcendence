ab6ecbbaed6675c8c74b2c3034455c2f5e6df6dd
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2023-08-19 17:01:27 +0200

    Fixed router, removed save-button from Settings

--- vue/conf/src/router/index.ts
+++ vue/conf/src/router/index.ts
@@ -124,6 +124,38 @@ const router = createRouter({
     //   ): void => jwtGuard(to, from, next)
     // },
     {
+      path: '/settings',
+      component: MainLayout,
+      children: [
+        {
+          path: '',
+          name: 'settings',
+          component: () => import('../views/SettingsView.vue') //lazy load
+        }
+      ],
+      beforeEnter: (
+        to: RouteLocationNormalized,
+        from: RouteLocationNormalized,
+        next: NavigationGuardNext
+      ): void => jwtGuard(to, from, next)
+    },
+    {
+      path: '/activity-center',
+      component: MainLayout,
+      children: [
+        {
+          path: '',
+          name: 'activity-center',
+          component: () => import('../views/ActivityCenterView.vue') //lazy load
+        }
+      ],
+      beforeEnter: (
+        to: RouteLocationNormalized,
+        from: RouteLocationNormalized,
+        next: NavigationGuardNext
+      ): void => jwtGuard(to, from, next)
+    },
+    {
       path: '/game/:matchId',
       component: MainLayout,
       children: [
