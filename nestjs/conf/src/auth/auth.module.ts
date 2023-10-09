41a522b81991d55a8568a282ee0a925198e31cd6
Halil ibrahim Kalyoncu <halil.kalyoncu@gmx.de>
2023-10-09 13:52:17 +0200

    removed: unused files; changed: access token is now 2 days valid

--- nestjs/conf/src/auth/auth.module.ts
+++ nestjs/conf/src/auth/auth.module.ts
@@ -22,7 +22,7 @@ import { ConnectedUserService } from '../chat/service/connected-user/connected-u
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '10000s' },
+        signOptions: { expiresIn: '2 days' },
       }),
     }),
   ],
